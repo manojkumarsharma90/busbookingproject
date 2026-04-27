4133f87c47b7f442d03c33eb463ac831b890c160
Himanshu-ABES <himanshu.22b1541059@abes.ac.in>
2026-04-27 12:29:30 +0530

    feat(entity): added relationships among bus customer and java

--- src/main/java/com/busbooking/entity/Driver.java
+++ src/main/java/com/busbooking/entity/Driver.java
@@ -1,16 +1,21 @@
 package com.busbooking.entity;
 import jakarta.persistence.*;
-import lombok.*;
 
 @Entity
 @Table(name = "drivers")
 
 public class Driver {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long driverId;
     private String licenseNumber;
     private String name;
     private String phone;
+    @ManyToOne @JoinColumn(name = "office_id")
+    private AgencyOffice office;
+    @ManyToOne @JoinColumn(name = "address_id")
+    private Address address;
+    
 	public Long getDriverId() {
 		return driverId;
 	}
@@ -35,12 +40,27 @@ public class Driver {
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
-	public Driver(Long driverId, String licenseNumber, String name, String phone) {
+	public AgencyOffice getOffice() {
+		return office;
+	}
+	public void setOffice(AgencyOffice office) {
+		this.office = office;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	public Driver(Long driverId, String licenseNumber, String name, String phone, AgencyOffice office,
+			Address address) {
 		super();
 		this.driverId = driverId;
 		this.licenseNumber = licenseNumber;
 		this.name = name;
 		this.phone = phone;
+		this.office = office;
+		this.address = address;
 	}
 	public Driver() {}
 }
