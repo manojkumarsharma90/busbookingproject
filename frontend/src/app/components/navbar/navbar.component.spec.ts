db062ba70327d764b0643defce34cbbc1304d15e
Raghav1021 <raghavrajbhatia123@gmail.com>
2026-05-04 16:00:07 +0530

    Added navbar component

---
+++ frontend/src/app/components/navbar/navbar.component.spec.ts
@@ -0,0 +1,34 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AuthService } from '../../services/auth.service';
+import { NavbarComponent } from './navbar.component';
+
+describe('NavbarComponent', () => {
+  let component: NavbarComponent;
+  let fixture: ComponentFixture<NavbarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RouterTestingModule, NavbarComponent],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: {
+            isAdmin: () => false,
+            isLoggedIn: () => false,
+            getUsername: () => 'Test User',
+            logout: () => {}
+          }
+        }
+      ]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(NavbarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule, RouterLink, RouterLinkActive],
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+  menuOpen = false;
+  constructor(public auth: AuthService) {}
+}
