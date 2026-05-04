fbed1683f2bfa5a89bcdd64ede3ec3f00db86018
manojkumarsharma90 <mukul2002sha14@gmail.com>
2026-05-04 16:35:59 +0530

    changes in app component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,14 +2,17 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ToastComponent } from './components/toast/toast.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FormsModule,CommonModule],
+  imports: [RouterOutlet,FormsModule,CommonModule,FooterComponent,NavbarComponent,ToastComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'frontendBusTicketBooking';
 }
